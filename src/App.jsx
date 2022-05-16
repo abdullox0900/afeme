b47f569f7e527651562923c8cd2a3977bcac8086
SayKham99 <x5292621@gmail.com>
2022-05-16 13:18:03 +0500

    Add divs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Footer from './Components/Footer/Footer';
 
 function App() {
-
+    return (
+        <div>
+            <Footer />
+        </div>
+    )
 }
 
 export default App;
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className='wrapper_footer'>
+                <div className='about_footer'>
+                </div>
+                <div className="apps_about">
+                </div>
+                <div className="links_footer">
+                </div>
+            </div>
+        )
+    }
+}
