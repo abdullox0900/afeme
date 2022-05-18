2131c2d50b5fb9d70ce5ccbe19ebf4761dbc2eb1
Uzcoin404 <uzcointg@gmail.com>
2022-05-18 11:09:54 +0500

    feat: add buttons, tooltips

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,12 @@
 import logo from "../../Assets/Img/logo.svg"
 import notificationIcon from "../../Assets/Img/notification.png"
 import loveIcon from "../../Assets/Img/love.png"
+import CurrencyIcon from "../../Assets/Img/currency.png"
 import Container from "../Container/Container";
 
-import { IconButton, Button } from '@mui/material';
+import { IconButton, Button, Tooltip, Grow } from '@mui/material';
 
 import "../Header/Header.scss";
-
-const theme = {
-    spacing: [0, 5, 10, 15, 20, 25, 30]
-}  
 
 function Header() {
     return (
@@ -18,20 +15,34 @@ function Header() {
                 <nav className="nav">
                     <Container>
                         <div className="nav_content">
-                            <a href="#" className="header__logo-link">
-                                <img className="header__logo-img" src={logo} alt="logo" />
-                            </a>
+                            <div className="nav_logo">
+                                <a href="#" className="header__logo-link">
+                                    <img className="header__logo-img" src={logo} alt="logo" />
+                                </a>
+                                <Tooltip title="Your location" arrow TransitionComponent={Grow}>
+                                    <Button className="btn nav_location" variant="text" sx={{py: 1, px: 1.5, ml: 1.5}} title="Set location">Uzbekistan</Button>
+                                </Tooltip>
+                            </div>
 
                             <div className="nav_items">
-                                <IconButton color="primary">
-                                    <img src={loveIcon} alt="" className="nav_icon nav_love" />
-                                </IconButton>
-                                <IconButton color="success" sx={{mr: 2}}>
-                                    <img src={notificationIcon} alt="" className="nav_icon nav_notification" />
-                                </IconButton>
+                                <Tooltip title="Your Currency" arrow TransitionComponent={Grow}>
+                                    <IconButton color="primary">
+                                        <img src={CurrencyIcon} alt="" className="nav_icon nav_currency" />
+                                    </IconButton>
+                                </Tooltip>
+                                <Tooltip title="Your Loves" arrow TransitionComponent={Grow}>
+                                    <IconButton color="primary">
+                                        <img src={loveIcon} alt="" className="nav_icon nav_love" />
+                                    </IconButton>
+                                </Tooltip>
+                                <Tooltip title="Notifications" arrow TransitionComponent={Grow}>
+                                    <IconButton color="success" sx={{mr: 2}}>
+                                        <img src={notificationIcon} alt="" className="nav_icon nav_notification" />
+                                    </IconButton>
+                                </Tooltip>
                                 <div className="nav_buttons" sx={{ml: 3}}>
-                                    <Button className="btn nav_button" variant="contained" sx={{p: 1}}>+ Eʻlon qoʻshish</Button>
-                                    <Button className="btn nav_button login_button" variant="text" sx={{ml: 2}}>Kirish</Button>
+                                    <Button className="btn nav_button" variant="contained" sx={{py: 1, px: 1.5}}>+ Eʻlon qoʻshish</Button>
+                                    <Button className="btn nav_button login_button" variant="text" sx={{ml: 2, py: 1.5, px: 2.5}}>Kirish</Button>
                                 </div>
                             </div>
                         </div>
