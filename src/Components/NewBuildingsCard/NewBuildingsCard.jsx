5e7dc9a248f555e80046ce901f39798a43a73567
abdullox0900 <abdullox0900@gmail.com>
2022-06-21 00:58:06 +0500

    feat: add reltor card skeleton && newBuilding cards skeleton and update

--- src/Components/NewBuildingsCard/NewBuildingsCard.jsx
+++ src/Components/NewBuildingsCard/NewBuildingsCard.jsx
@@ -4,41 +4,66 @@ import React, { useEffect, useState } from "react";
 // Import => Style Component
 import "../../Components/NewBuildingsCard/NewBuildingsCard.scss";
 
-// Import => Componnets Img
-
 // Import => Axios
 import axios from "axios";
+
+// Import => Skeleton
+import ContentLoader from "react-content-loader";
 
 function NewBuildingsCard() {
 
     const [regionData, setRegionData] = useState([]);
+    const [isLoading, setLoading] = useState(false);
 
     useEffect(() => {
         axios.get(`https://ali98.uz/api/regions`)
             .then(res => {
                 const resdata = res.data.data;
                 setRegionData(resdata)
+                setLoading(true)
             })
     }, [])
 
-    console.log(regionData)
+    const isLodArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
     return (
         <>
             <div className="new-buildin-wrap-card">
                 {
-                    regionData.map((reg) => {
-                        return (
-                            <div className="new-buildin-card">
-                                <img className="new-buildin-card__img" src={reg.image} alt="" />
+                    isLoading ? (
 
-                                <div className="new-buildin-card__body">
-                                    <h4 className="new-buildin-card__title">{reg.name_uz}</h4>
+                        isLodArr.map((lod) => {
+                            return (
+                                <ContentLoader
+                                    speed={2}
+                                    width={400}
+                                    height={260}
+                                    viewBox="0 0 400 260"
+                                    backgroundColor="#f3f3f3"
+                                    foregroundColor="#ecebeb">
+                                    <rect x="88" y="229" rx="3" ry="3" width="150" height="15" />
+                                    <circle cx="46" cy="236" r="21" />
+                                    <rect x="19" y="47" rx="0" ry="0" width="350" height="150" />
+                                </ContentLoader>
+                            )
+                        })
+                    ) : (
+
+                        regionData.map((reg) => {
+                            return (
+                                <div className="new-buildin-card">
+                                    <img className="new-buildin-card__img" src={reg.image} alt="" />
+
+                                    <div className="new-buildin-card__body">
+                                        <h4 className="new-buildin-card__title">{reg.name_uz}</h4>
+                                    </div>
                                 </div>
-                            </div>
-                        )
-                    })
+                            )
+                        })
+                    )
                 }
+
+
             </div>
         </>
     )
