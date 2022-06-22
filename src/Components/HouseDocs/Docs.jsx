5da015a67a6284f7e9e9b5402e3001ad6e8c9a53
SayKham99 <x5292621@gmail.com>
2022-06-22 14:04:52 +0500

    feat: complete sending video and images

--- src/Components/HouseDocs/Docs.jsx
+++ src/Components/HouseDocs/Docs.jsx
@@ -1,17 +1,33 @@
 // Import => React
+import axios from 'axios';
 import React from 'react';
 
 // Import => Components
 import style from './Docs.module.scss';
 
 function Docs({ docs, setDocs }) {
-  function onChange(e) {
-    let files = e.target.files[0]
-    setDocs([files])
+  const arr = new Array();
+  function dropImageHandler(e) {
+    var formdata = new FormData();
+    const files = e
+    console.log(e.target.value);
+    formdata.append('key', 'Service For C Group')
+    formdata.append("file", e.target.value);
+    axios.post('http://ali98.uz/api/service', formdata)
+      .then(function (response) {
+        console.log(response.data)
+      })
+      .catch(function (res) {
+        console.log(res.response.data.message);
+      })
+    setDocs(files)
   }
 
   return (
-    <input className={style.inp} type="file" onChange={(e) => onChange(e)} />
+    <>
+      <label htmlFor="file">Ali</label>
+      <input type="file" id='file' onChange={(e) => console.log(e)} />
+    </>
   )
 }
 
