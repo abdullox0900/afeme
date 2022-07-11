0b8f374c716e31130381955592d914c38ea8f6ac
SayKham99 <x5292621@gmail.com>
2022-07-11 14:27:09 +0500

    feat:done crud personal cabinet

--- src/Components/UserProfil/UserProfil.jsx
+++ src/Components/UserProfil/UserProfil.jsx
@@ -5,65 +5,79 @@ import React, { useContext, useEffect, useState } from "react";
 import Container from "../../Components/Container/Container";
 import UserProfilList from "../UserProfilList/UserProfilList";
 
-import { Context } from "../../Context/LangContext";
 import { UserContext } from "../../Context/UserContext";
-
-// Import => Img
-import UserIlustration from "../../Assets/Img/userIlutration.svg"
 
 // Import => Style Component
 import "../../Components/UserProfil/UserProfil.scss";
 import axios from "axios";
+import { FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 
 function UserProfil() {
-    const { user, setUser } = useContext(UserContext);
+    const { user } = useContext(UserContext);
     const [name, setName] = useState('')
     const [lastname, setLastName] = useState('')
     const [phone, setPhone] = useState('')
     const [email, setEmail] = useState('')
     const [passport, setPassport] = useState('')
     const [region, setRegion] = useState('')
-    const [posts, setPosts] = useState('')
     const [type, setType] = useState('')
+    const [uniq, setUniq] = useState('')
+    const [regions, setRegions] = useState([]);
 
     useEffect(() => {
-        setName(user.data?.name);
-        setLastName(user.data?.lastname);
-        setPhone(user.data?.phone);
-        setEmail(user.data?.email);
-        setPassport(user.data?.passport);
-        setRegion(user.data?.region_id?.name_uz);
-        setPosts(user.data?.posts);
-        setType(user.data?.user_type);
-    }, [])
+        if (user.hasOwnProperty('data')) {
+            setName(user.data.name);
+            setLastName(user.data.lastname);
+            setPhone(user.data.phone);
+            setEmail(user.data.email);
+            setPassport(user.data.pasport);
+            setType(user.data.user_type);
+            setRegion(user.data.region_id.id);
+            setUniq(user.data.id);
+        }
+    }, [user]);
 
     let token = localStorage.getItem('Token')
     let all = new URLSearchParams();
     all.append('name', name)
     all.append('lastname', lastname)
     all.append('phone', phone)
-    all.append('email', 'sıdhfıusdhfıo') 
+    all.append('email', email)
     all.append('passport', passport)
-    all.append('region_id', user.data?.region_id?.id)
-    all.append('user_type', 'companiya')
+    all.append('user_type', type)
+    all.append('region_id', region)
 
     let headersList = {
         "Accept": "*/*",
-        "User-Agent": "Thunder Client (https://www.thunderclient.com)",
         'Authorization': `Bearer ${token}`
-
     }
     const Put = (e) => {
         fetch(`https://ali98.uz/api/user/${user.data.id}?paremeter=PUT`, {
             method: "PUT",
             headers: headersList,
-            body:all
+            body: all
         }).then(function (response) {
             return response.text();
         }).then(function (data) {
             console.log(data);
+            window.location.reload()
         })
     }
+
+    useEffect(() => {
+        const regions = async () => {
+            try {
+                const res = await axios.get('https://ali98.uz/api/regions');
+                if (res) {
+                    let data = res.data.data
+                    setRegions(data)
+                }
+            } catch (error) {
+                console.log("error")
+            }
+        }
+        regions();
+    }, [])
 
     return (
         <>
@@ -71,25 +85,81 @@ function UserProfil() {
                 <div className="user-profil-wrap">
                     <UserProfilList />
                     <div className="personal">
-                        <div className="inpG">
-                            <label htmlFor="name">Name</label>
-                            <input type="text" value={name} id={name} />
-                            <label htmlFor="lastname">LastName</label>
-                            <input type="text" value={lastname} id={lastname} />
-                            <label htmlFor="phone">Phone Number</label>
-                            <input type="text" value={phone} id={phone} />
-                            <label htmlFor="email">Email</label>
-                            <input type="text" value={email} id={email} />
-                            <label htmlFor="passport">Passport</label>
-                            <input type="text" value={passport} id={passport} />
-                            <label htmlFor="region">Region</label>
-                            <input type="text" value={region} id={region} />
-                            <label htmlFor="posts">Postlar Soni</label>
-                            <input type="text" value={posts?.length} id={posts} />
-                            <label htmlFor="type">Mijor Turi</label>
-                            <input type="text" value={type} id={type} />
+                        <div className="title">
+                            <p>ID//{uniq}</p>
+                            <button type="submit" onClick={(e) => Put(e)}>Edit</button>
                         </div>
-                        <button type="submit" onClick={(e) => Put(e)}>Edit</button>
+                        <div className="inpG">
+                            <TextField
+                                label='Name'
+                                id="outlined-basic"
+                                variant="outlined"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                            <TextField
+                                label='LastName'
+                                id="outlined-basic"
+                                variant="outlined"
+                                value={lastname}
+                                onChange={(e) => setLastName(e.target.value)}
+                            />
+                            <TextField
+                                label='phone'
+                                id="outlined-basic"
+                                variant="outlined"
+                                value={phone}
+                                onChange={(e) => setPhone(e.target.value)}
+                            />
+                            <TextField
+                                label='Email'
+                                id="outlined-basic"
+                                variant="outlined"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <TextField
+                                label='Passport'
+                                id="outlined-basic"
+                                variant="outlined"
+                                value={passport}
+                                onChange={(e) => setPassport(e.target.value)}
+                            />
+                            <FormControl className="form__controler-input2">
+                                <InputLabel id="viloyat"> Hudud </InputLabel>
+                                <Select
+                                    labelId="viloyat"
+                                    id="viloyat"
+                                    label='Hudud'
+                                    value={region}
+                                    onChange={(e) => setRegion(e.target.value)}
+                                >
+                                    {regions.map((region) => (
+                                        <MenuItem
+                                            key={region.id}
+                                            value={region.id}
+                                        >
+                                            {region.name_uz}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                            <FormControl className="form__controler-input1">
+                                <InputLabel id="demo-simple-select-label">Type</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={type}
+                                    label='Type'
+                                    onChange={(e) => setType(e.target.value)}
+                                >
+                                    <MenuItem value={'mijoz'}>Mijoz</MenuItem>
+                                    <MenuItem value={'rieltor'}>Rieltor</MenuItem>
+                                    <MenuItem value={'companiya'}>Companiya</MenuItem>
+                                    <MenuItem value={'quruvchi firma'}>Quruvchi firma</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </div>
                     </div>
                 </div>
             </Container>
